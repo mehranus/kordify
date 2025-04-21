fc365369dc02f41cf399238844f4ba853b2e722e
mehran feriduni <m3hr4nus@gmail.com>
2025-04-21 18:09:15 +0330

    new project start

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app/app.module';
+import {  ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
+import { join } from 'path';
+import { swagerConfigInit } from './config/swagger.config';
+
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
+  config({path:join(process.cwd(),'.env')})
+  swagerConfigInit(app)
+  const {PORT}=process.env
+  await app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`);
+    console.log(`swagger: http://localhost:${PORT}/swagger`);
+  });
+}
+bootstrap();
+
+
